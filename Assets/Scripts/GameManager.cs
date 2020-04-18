8d6c8c2aa849a46126f47c6af4801298c917d503
Artjoms-Jakovenko <artjoms.jakovenko@gmail.com>
2020-04-18 20:55:46 +0200

    Drawn icons for reward screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -66,7 +66,7 @@ public class GameManager : MonoBehaviour
                     LevelManager.RecordLevelCompletion(SceneManager.GetActiveScene().name, levelCompleted, starsAchieved); // TODO add possibility to fail a level and stars on achievements
                     
                     rewardScreen.SetActive(true);
-                    rewardScreen.GetComponent<RewardScreen>().RenderRewards();
+                    rewardScreen.GetComponent<RewardScreen>().RenderRewardScreen();
                     #endregion
                     //}
                 }
--- Assets/Scripts/Rewards/RewardScreen.cs
+++ Assets/Scripts/Rewards/RewardScreen.cs
@@ -11,7 +11,15 @@ public class RewardScreen : MonoBehaviour
     public GameObject rewardContent;
 
     private GameObject rewardBlockAsset;
+
     private Dictionary<InventoryEnum, int> rewards;
+    private int starsAchieved = 0;
+    bool completed = false;
+
+    private Image leftStar;
+    private Image centerStar;
+    private Image rightStar;
+    private Sprite orangeStar;
 
     LevelEnum nextLevel;
     private void OnEnable()
@@ -26,11 +34,17 @@ public class RewardScreen : MonoBehaviour
     private void Awake()
     {
         rewardBlockAsset = Resources.Load<GameObject>("UI/RewardBlock");
+        leftStar = transform.Find("RewardScreenCanvas/Background/LeftStar").GetComponent<Image>();
+        centerStar = transform.Find("RewardScreenCanvas/Background/CenterStar").GetComponent<Image>();
+        rightStar = transform.Find("RewardScreenCanvas/Background/RightStar").GetComponent<Image>();
+        orangeStar = Resources.Load<Sprite>("UI/LevelGoals/StarIcon");
     }
 
     public void AdministerRewards(LevelEnum levelName, bool completed, int starsAchieved)
     {
         rewards = LevelInfoData.GetLevelRewards(levelName, completed, LevelManager.GetLevelProgress(levelName).starCount, starsAchieved);
+        this.starsAchieved = starsAchieved;
+        this.completed = completed;
 
         foreach (var reward in rewards)
         {
@@ -40,8 +54,10 @@ public class RewardScreen : MonoBehaviour
         SaveSystem.Save();
     }
 
-    public void RenderRewards()
+    public void RenderRewardScreen()
     {
+        SetStarColor();
+
         foreach (var reward in rewards)
         {
             GameObject rewardBlock = GameObjectUtility.InstantiateChild(rewardBlockAsset, rewardContent, true);
@@ -61,5 +77,26 @@ public class RewardScreen : MonoBehaviour
     public void NextLevel()
     {
         LevelManager.Load(nextLevel, true);
+    }
+
+    private void SetStarColor()
+    {
+        if(starsAchieved >= 1)
+        {
+            leftStar.sprite = orangeStar;
+            leftStar.color = Color.white;
+        }
+
+        if(starsAchieved >= 2)
+        {
+            centerStar.sprite = orangeStar;
+            centerStar.color = Color.white;
+        }
+
+        if(starsAchieved >= 3)
+        {
+            rightStar.sprite = orangeStar;
+            rightStar.color = Color.white;
+        }
     }
 }
